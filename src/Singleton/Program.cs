72842313ef9b7529c56b03ab64976c91b5806bc3
rubone <rubone02@gmail.com>
2024-01-11 17:46:56 -0600

    :construction: Use singleton class in program

--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Singleton;
+
+LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+if (b1 == b2 && b2 == b3 && b3 == b4)
+{
+    Console.WriteLine("Same instance\n");
+}
+
+LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+for (int i = 0; i < 15; i++)
+{
+    string serverName = balancer.Server;
+    Console.WriteLine("Dispatch Request to: " + serverName);
+}
+
+Console.ReadKey();
